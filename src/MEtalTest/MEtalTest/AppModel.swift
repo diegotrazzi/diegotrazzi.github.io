8636993c572618b3f8ca81d35d246ab630731a6e
Diego Trazzi <dtrazzi@apple.com>
2024-11-04 10:32:00 -0800

    Added MetalTestApp

---
+++ src/MEtalTest/MEtalTest/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  MEtalTest
+//
+//  Created by mac_sys1 on 11/2/24.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ src/MEtalTest/MEtalTest/MEtalTestApp.swift
@@ -0,0 +1,45 @@
+//
+//  MEtalTestApp.swift
+//  MEtalTest
+//
+//  Created by mac_sys1 on 11/2/24.
+//
+
+import SwiftUI
+import CompositorServices
+
+struct ContentStageConfiguration: CompositorLayerConfiguration {
+    func makeConfiguration(capabilities: LayerRenderer.Capabilities, configuration: inout LayerRenderer.Configuration) {
+        configuration.depthFormat = .depth32Float
+        configuration.colorFormat = .rgba16Float
+
+        let foveationEnabled = capabilities.supportsFoveation
+        configuration.isFoveationEnabled = foveationEnabled
+
+        let options: LayerRenderer.Capabilities.SupportedLayoutsOptions = foveationEnabled ? [.foveationEnabled] : []
+        let supportedLayouts = capabilities.supportedLayouts(options: options)
+
+        configuration.layout = supportedLayouts.contains(.layered) ? .layered : .dedicated
+    }
+}
+
+@main
+struct MEtalTestTestApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            CompositorLayer(configuration: ContentStageConfiguration()) { @MainActor layerRenderer in
+                Renderer.startRenderLoop(layerRenderer, appModel: appModel)
+            }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+    }
+}
+
